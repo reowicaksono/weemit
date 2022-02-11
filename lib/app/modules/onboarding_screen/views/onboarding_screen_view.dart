9d871e45a085bcfe44ce84285002bd775ff141a1
reowicaksono <reo.wicaksono.2005356@gmail.com>
2022-02-11 10:53:28 +0700

    add on boarding screen with lottie

--- lib/app/modules/onboarding_screen/views/onboarding_screen_view.dart
+++ lib/app/modules/onboarding_screen/views/onboarding_screen_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'package:lottie/lottie.dart';
 
 import '../controllers/onboarding_screen_controller.dart';
 
@@ -8,14 +10,73 @@ class OnboardingScreenView extends GetView<OnboardingScreenController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('OnboardingScreenView'),
-        centerTitle: true,
-      ),
       body: Center(
-        child: Text(
-          'OnboardingScreenView is working',
-          style: TextStyle(fontSize: 20),
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+              title: "Chatting with your firends",
+              body:
+                  "You can search many weaboo in the world and chat with them",
+              image: Container(
+                height: Get.width * 0.4,
+                width: Get.width * 0.4,
+                child: Center(child: Lottie.asset("assets/lottie/send.json")),
+              ),
+              decoration: const PageDecoration(
+                titleTextStyle: TextStyle(color: Colors.black, fontSize: 24.0,fontWeight: FontWeight.w700),
+                bodyTextStyle:
+                    TextStyle(fontWeight: FontWeight.w300, fontSize: 18.0),
+              ),
+            ),
+            PageViewModel(
+              title: "Chatting with your firends",
+              body:
+                  "You can search many weaboo in the world and chat with them",
+              image: Container(
+                height: Get.width * 0.4,
+                width: Get.width * 0.4,
+                child: Center(child: Lottie.asset("assets/lottie/anime.json")),
+              ),
+              decoration: const PageDecoration(
+                titleTextStyle: TextStyle(color: Colors.orange),
+                bodyTextStyle:
+                    TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+              ),
+            ),
+            PageViewModel(
+              title: "Chatting with your firends",
+              body:
+                  "You can search many weaboo in the world and chat with them",
+              image: Container(
+                height: Get.width * 0.4,
+                width: Get.width * 0.4,
+                child: Center(child: Lottie.asset("assets/lottie/video.json")),
+              ),
+              decoration: const PageDecoration(
+                titleTextStyle: TextStyle(color: Colors.orange),
+                bodyTextStyle:
+                    TextStyle(fontWeight: FontWeight.w700, fontSize: 20.0),
+              ),
+            ),
+          ],
+          onDone: () {
+            // When done button is press
+          },
+          onSkip: () {
+            // You can also override onSkip callback
+          },
+          showSkipButton: true,
+          skip: const Icon(Icons.skip_next),
+          next: Text("Next"),
+          done:
+              const Text("Done", style: TextStyle(fontWeight: FontWeight.w600)),
+          dotsDecorator: DotsDecorator(
+              size: const Size.square(10.0),
+              activeSize: const Size(20.0, 10.0),
+              color: Colors.black26,
+              spacing: const EdgeInsets.symmetric(horizontal: 3.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(25.0))),
         ),
       ),
     );
--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -3,6 +3,8 @@ import 'package:get/get.dart';
 import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/onboarding_screen/bindings/onboarding_screen_binding.dart';
+import '../modules/onboarding_screen/bindings/onboarding_screen_binding.dart';
+import '../modules/onboarding_screen/views/onboarding_screen_view.dart';
 import '../modules/onboarding_screen/views/onboarding_screen_view.dart';
 import '../modules/splash_screen/bindings/splash_screen_binding.dart';
 import '../modules/splash_screen/views/splash_screen_view.dart';
@@ -29,6 +31,13 @@ class AppPages {
       name: _Paths.ONBOARDING_SCREEN,
       page: () => OnboardingScreenView(),
       binding: OnboardingScreenBinding(),
+      children: [
+        GetPage(
+          name: _Paths.ONBOARDING_SCREEN,
+          page: () => OnboardingScreenView(),
+          binding: OnboardingScreenBinding(),
+        ),
+      ],
     ),
   ];
 }
